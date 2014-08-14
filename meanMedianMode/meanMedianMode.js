8217f16d72aa2cb757b986b6a67a85b6db3ce09c
David Davidson <daviddavidson.1988@gmail.com>
2014-08-13 20:37:30 -0700

    Add block commenting

--- meanMedianMode/meanMedianMode.js
+++ meanMedianMode/meanMedianMode.js
@@ -1,3 +1,11 @@
+/**
+ * This function accepts an array of one or more numbers and returns
+ * their mean.
+ *
+ * @param {Array} numbers
+ * @return {Number} The mean of the array's contents
+ */
+
 var mean = function(numbers) {
 	var meanSum = 0;
 	for (var i = 0; i < numbers.length; i++) {
@@ -6,21 +14,37 @@ var mean = function(numbers) {
 	return (meanSum / numbers.length);
 };
 
+/**
+ * This function accepts an array of one or more numbers and returns
+ * their median: either the "middle" number, or (if the array's length is even)
+ * the mean of the middle two.
+ *
+ * @param {Array} numbers
+ * @return {Number} The median of the array's contents
+ */
+
 var median = function(numbers) {
 	var sortedNumbers = numbers.slice().sort(function(a, b) { 
 		return a - b;  // .slice() lets us sort a copy, not the original: no side effects
 	});
 	if (sortedNumbers.length % 2 !== 0) {
-		// Odd number of arguments? Return the middle number...
 		var middle = (sortedNumbers.length - 1) / 2;
 		return sortedNumbers[middle]; 
 	} else {
-		// ...Even? Return the mean of the middle two
 		var lowerMiddle = Math.floor((sortedNumbers.length - 1) / 2);
 		var higherMiddle = Math.ceil((sortedNumbers.length - 1) / 2);
 		return mean([sortedNumbers[lowerMiddle], sortedNumbers[higherMiddle]]);
 	}
 };
+
+/**
+ * This function accepts an array of one or more numbers and returns
+ * their mode, or--if there's more than one mode--all the "winners."
+ *
+ * @param {Array} numbers
+ * @return {Number or String} The mode of the array's contents, or (if there's)
+ * no single winner) a string naming all the modes
+ */
 
 var mode = function(numbers) {
 	var numAppearances = [], winners = [], prevMax = 0;
@@ -44,7 +68,6 @@ exports.mean = mean;
 exports.median = median;
 exports.mode = mode;
 
-
 // Start of execution
 
 // Sanitize input
@@ -59,7 +82,7 @@ if (process.argv.length < 1) {
 	return false;
 }
 
-// Let's do some math!
+// Now let's do some math!
 console.log('mean: ' + mean(process.argv));
 console.log('median: ' + median(process.argv));
 console.log('mode: ' + mode(process.argv));
