59edc809d31f1f3e3359760d52157a5d00b80303
David Davidson <daviddavidson.1988@gmail.com>
2014-08-14 08:06:43 -0700

    Moving the valid-numbers check into its own file, so others can use it

--- meanMedianMode/meanMedianMode.js
+++ meanMedianMode/meanMedianMode.js
@@ -1,3 +1,5 @@
+var validNumbers = require('../validNumbers');
+
 /**
  * This function accepts an array of one or more numbers and returns
  * their mean.
@@ -71,14 +73,7 @@ exports.mode = mode;
 // Start of execution
 
 // Sanitize input
-for (var i = 0; i < process.argv.length; i++) {
-	if (isNaN(process.argv[i])) { // (Removes "node" and "~/**/numbers.js", too)
-		process.argv.splice(i, 1);
-		i--; // So that the array doesn't "shift" and hide one argument
-	}
-}
-if (process.argv.length < 1) {
-	console.log('Uh oh, you didn\'t give me any numbers!');
+if (!validNumbers()) {
 	return false;
 }
 
