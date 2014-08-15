5adfc184f3332f1404fd53cbf70b9c2f6234cd0b
David Davidson <daviddavidson.1988@gmail.com>
2014-08-15 15:36:38 -0700

    Expand linting to test directories

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt){
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jshint: {
-            files: ['*/*.js']
+            files: ['sorts/**/*.js', 'tests/**/*.js']
         }
     });
 grunt.registerTask('default', [
