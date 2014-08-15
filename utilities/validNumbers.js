333aac8b8a1b4c741842013af3bbbace317534ea
David Davidson <daviddavidson.1988@gmail.com>
2014-08-15 14:59:49 -0700

    Map each passing element to Number

--- utilities/validNumbers.js
+++ utilities/validNumbers.js
@@ -5,6 +5,7 @@ module.exports = function() {
 			i--; // So that the array doesn't "shift" and hide one argument
 		}
 	}
+	process.argv = process.argv.map(Number);
 	if (process.argv.length < 1) {
 		console.log('Uh oh, you didn\'t give me any numbers!');
 		return false;
