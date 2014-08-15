2e330373a69838ac3c048d442c3db353a1ec9d58
David Davidson <daviddavidson.1988@gmail.com>
2014-08-15 15:37:43 -0700

    Add bubble-sort bin and lib

---
+++ sorts/bubbleSort/lib/bubbleSort.js
@@ -0,0 +1,27 @@
+var exchange = require('../../../utilities/exchange');
+
+/**
+ * This function accepts an array of numbers and sorts them using bubble sort.
+ *
+ * How it works: In the inner loop, move through the array pair by pair, and 
+ * swap any two that are out of order. Keep doing this over and over (governed
+ * by the outer loop) until the array is sorted.
+ *
+ * Performance: On^2; no real reason to use it.
+ *
+ * @param {Array} numbers
+ * @return {Array} the sorted array
+ */
+
+var bubbleSort = function(numbers) {
+	for (var i = 0; i < numbers.length; i++) {
+		for (var j = 0; j < numbers.length - i; j++) {
+			if (numbers[j] > numbers[j + 1]) {
+				exchange(numbers, j, j + 1);
+			}
+		}
+	}
+	return numbers;
+};
+
+exports.bubbleSort = bubbleSort;
