99066b401c380e986ac127fc63e7e117b920a560
David Davidson <daviddavidson.1988@gmail.com>
2014-10-30 16:44:59 -0700

    Move to , not , as the name of the object that exposes sandboxed methods

--- tests/patterns/sandboxTests.js
+++ tests/patterns/sandboxTests.js
@@ -1,41 +1,37 @@
 'use strict';
 
 var Sandbox = require('../../lib/patterns/sandbox'),
-	expect = require('chai').expect;
+	expect = require('chai').expect,
+	utils;
 
 describe('The sandbox pattern', function() {
 	
 	it('Protects the global scope', function() {
-		
-		new Sandbox('events', function(box) {
-			
-			// Do stuff with the `events` methods
-			// Variables defined here won't reach the global scope
-
-			expect(typeof box.addListener).to.eql('function');
-			var privateVar = box.addListener();
+		new Sandbox(function(utils) {
+			expect(typeof utils.addListener).to.eql('function');
+			var privateVar = utils.addListener();
 			expect(privateVar).to.eql('Just added an event listener');
 		});
 
 		expect(function() {
-			box.addListener(); // jshint ignore:line
+			utils.addListener(); // jshint ignore:line
 		}).to.throw(Error);
 
 		expect(typeof privateVar).to.eql('undefined');
-
+		expect(typeof utils).to.eql('undefined');
 	});
 
 	it('Lets me expose the methods I want and hide the ones I don\'t', function() {
-		new Sandbox('dom', function(box) {
-			expect(typeof box.addListener).to.eql('undefined');
-			expect(typeof box.addDomElement).to.eql('function');
+		new Sandbox('dom', function(utils) {
+			expect(typeof utils.addListener).to.eql('undefined');
+			expect(typeof utils.addDomElement).to.eql('function');
 		});
 	});
 
 	it('Exposes all its methods if I don\'t ask for any in particular', function() {
-		new Sandbox(function(box) {
-			expect(typeof box.addListener).to.eql('function');
-			expect(typeof box.addDomElement).to.eql('function');
+		new Sandbox(function(utils) {
+			expect(typeof utils.addListener).to.eql('function');
+			expect(typeof utils.addDomElement).to.eql('function');
 		});
 	});
 
