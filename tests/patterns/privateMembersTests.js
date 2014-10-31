aa01e9cebb3416834d94fbe5afd3c51034dd6d3d
David Davidson <daviddavidson.1988@gmail.com>
2014-10-31 09:29:29 -0700

    Move the old IIFE pattern into a more complete privateMembers example

---
+++ tests/patterns/privateMembersTests.js
@@ -0,0 +1,50 @@
+'use strict';
+
+var privateMembers = require('../../lib/patterns/privateMembers'),
+	hasSecrets = privateMembers.hasSecrets,
+	checkGlobalScope = privateMembers.checkGlobalScope,
+	expect = require('chai').expect;
+
+describe('Private properties and methods', function() {
+	it('Hide data from the global scope', function() {
+		expect(checkGlobalScope).to.throw(Error);
+	});
+
+	it('Allow privileged read access to private properties', function() {
+		expect(hasSecrets.seePrimitive()).to.eql('a string');
+	});
+
+	it('Allow privileged write access to private properties', function() {
+		hasSecrets.updatePrimitive('something new');
+		expect(hasSecrets.seePrimitive()).to.eql('something new');
+	});
+
+	describe('Vulnerability: returning private objects directly (by reference)', function() {
+		it('Lets the outside world modify them', function() {
+			var outsideWorld,
+				anotherInstance;
+
+			outsideWorld = hasSecrets.seeObjectNaive();
+			expect(outsideWorld.characteristics).to.eql('secret');
+			outsideWorld.characteristics = 'not so secret anymore!';
+
+			anotherInstance = hasSecrets.seeObjectNaive();
+			expect(anotherInstance.characteristics).to.eql('not so secret anymore!');
+			outsideWorld.characteristics = 'secret';
+		});
+	});
+
+	describe('Solution: pass back a brand-new object', function() {
+		it('Protects the private data', function() {
+			var outsideWorld,
+				anotherInstance;
+
+			outsideWorld = hasSecrets.seeObjectSecure();
+			expect(outsideWorld.characteristics).to.eql('secret');
+			outsideWorld.characteristics = 'not so secret anymore!';
+
+			anotherInstance = hasSecrets.seeObjectSecure();
+			expect(anotherInstance.characteristics).to.eql('secret');
+		});
+	});
+});
